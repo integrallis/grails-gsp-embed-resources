38d330ff12a856df5a6a7cac3c80897783163c99
Joseph Nusairat <jnusairat@integrallis.com>
2012-06-04 21:16:48 -0700

    Added svn resource url

--- GspEmbedResourcesGrailsPlugin.groovy
+++ GspEmbedResourcesGrailsPlugin.groovy
@@ -43,7 +43,7 @@ ie :
     def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def organization = [ name: "Integrallis Software", url: "http://www.integrallis.com/" ]
 
     // Any additional developers beyond the author specified above.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
@@ -52,7 +52,7 @@ ie :
 //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+    def scm = [ url: "https://github.com/integrallis/grails-gsp-embed-resources" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
